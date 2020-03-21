8b05064ee7d7987184debe46d8dd0bf6ed6f5707
Rached Mejri <r.mejri74100@gmail.com>
2020-03-21 21:17:27 +0100

    revert initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,40 @@
-use std::fs::File;
 use std::env;
-use std::io::Read;
+use std::fs::File;
 use std::io;
+use std::io::Read;
 
 fn interpret(code: &str, cells: &mut Vec<i32>, index: &mut usize) {
     for (i, elem) in code.split("").enumerate() {
-        if elem == ">" {
-            *index += 1;
-
-            if cells.get(*index).is_none() {
-                cells.push(0)
+        match elem {
+            ">" => {
+                *index += 1;
+                if cells.get(*index).is_none() {
+                    cells.push(0)
+                }
             }
-        }
 
-        if elem == "<" {
-            *index -= 1
-        }
-
-        if elem == "+" {
-            cells[*index] += 1;
-        }
-
-        if elem == "-" {
-            cells[*index] -= 1;
-        }
-
-        if elem == "[" {
-            return interpret(&code[i..], cells, index);
-        }
-
-        if elem == "]" {
-            return if cells[*index] == 0 {
-                interpret(&code[i..], cells, index)
-            } else {
-                interpret(&code, cells, index)
+            "<" => *index -= 1,
+            "+" => {
+                cells[*index] += 1;
             }
-        }
+            "-" => {
+                cells[*index] -= 1;
+            }
+            "[" => {
+                return interpret(&code[i..], cells, index);
+            }
+            "]" => {
+                return if cells[*index] == 0 {
+                    interpret(&code[i..], cells, index)
+                } else {
+                    interpret(&code, cells, index)
+                }
+            }
+            "." => {
+                println!("[{}]", cells[*index]);
+            }
 
-        if elem == "." {
-            println!("[{}]",  cells[*index]);
+            _ => (),
         }
     }
 }
@@ -46,7 +42,7 @@ fn interpret(code: &str, cells: &mut Vec<i32>, index: &mut usize) {
 fn main() -> io::Result<()> {
     let mut buffer = String::new();
     let args = env::args().collect::<Vec<String>>();
-     File::open(&args[1])?.read_to_string(&mut buffer)?;
+    File::open(&args[1])?.read_to_string(&mut buffer)?;
     interpret(&buffer, &mut vec![0], &mut 0);
     Ok(())
 }
